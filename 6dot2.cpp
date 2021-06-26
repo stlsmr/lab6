a22943e3efa10391d3207e1d855aa5365d9c2763
stlsmr <81382999+stlsmr@users.noreply.github.com>
2021-06-26 11:12:08 +0300

    Update 6dot2.cpp

--- 6dot2.cpp
+++ 6dot2.cpp
@@ -1,38 +1,65 @@
-
 #include <iostream>
-#include <algorithm>
-#include <string>
-#include <stdlib.h>
-
 using namespace std;
 
-int predel(int fpred, int spred) {
+void menu(int& a, int& b, int& c) {
+    int d = 0; //для выбора варианта второго меню
+    while (d != 4) { // цикл для запуска выбора
+        cout << "1 - Ввод верхнего предела " << endl;
+        cout << " Число - " << a << endl; // вывод существующего числа
+        cout << "2 - Ввод нижнего предела " << endl;
+        cout << " Число - " << b << endl; // вывод существующего числа
+        cout << "3 - Изменить число " << endl;
+        cout << " Число - " << c << endl; // вывод существующего числа
+        cout << "4 - Выйти " << endl;
+        cout << "Выберите действие ";
+        cin >> d;
+        switch (d) {
+            case(1):
+                cout << "Изменить "; // изменение верхнего предела
+                cin >> a;
+                cout << endl;
+                break;
+            case(2):
+                cout << "Изменить "; // изменение нижнего предела
+                cin >> b;
+                cout << endl;
+                break;
+            case(3):
+                cout << "Изменить входящее число на - "; // изменение числа
+                cin >> c;
+                cout << endl;
+                break;
+        }
+        if (d == 4) exit;
+    }
+}
 
-    double z;
-    cin >> z; //считываем число
-    if (z < fpred || z > spred || z == (long)z)
-        def(&fpred, &spred, &z);
-    else return z;
+void vvod(int& a, int& b) { //функция ввода числа
+    int c;
+    cout << "Введи входящее число" << endl;
+    cin >> c;
+    while (true) { // истина
+        if (a < c && c < b) {
+            cout << "Число входит в интервал" << endl;
+            break;
+        }
+        else (false); // ложь
+        {
+            menu(a, b, c);
+        }
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int a, b;
+    cout << "Меню" << endl;
+    cout << "*********************************************" << endl;
+    vvod(a, b);
+    cout << "*********************************************" << endl;
+    system("pause");
+    return 0;
 }
 
 
-int def(int* fpred, int* spred, double* z) {
-    cout << "\nДоступные действия:\n\n";
-
-    cout << "1: Ввод верхнего предела\t";
-    cout << "2: Ввод нижнего предела\n";
-    cout << "3: Повторный ввод числа\t";
-    cout << "Ввод любого иного варианта для выхода из программы";
-    int k;
-    cin >> k;
-    switch (k) {
-        case 1:
-            cin >> *fpred;
-        case 2:
-            cin >> *spred;
-        case 3:
-            cin >> *z;
-        default:
-            exit(1);
-    };
-}
